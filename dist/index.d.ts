5508d5f4f00d85f1a423d3951534fcdce69ea322
Max Kim <hwaseen@gmail.com>
2020-11-30 23:19:27 +0900

    setting:add deploy command in package.json

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,2 @@
-/**
-* @Method: Returns the plural form of any noun.
-* @Param: {string}
-* @Return: {string}
-*/
-export declare function getPlural(str: any): string;
+declare const root: () => void;
+export default root;

c57461d2815217a2d879f7efdb84e0cfea3553b5
Max Kim <hwaseen@gmail.com>
2020-11-30 23:13:07 +0900

    setting:change yarn to padckage-lock.json, modify index

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
-import * as pluralize from 'pluralize'
-
-/**
-* @Method: Returns the plural form of any noun.
-* @Param: {string}
-* @Return: {string}
-*/
-export function getPlural(str: any): string {
-  return pluralize.plural(str)
+const root = () => {
+  console.log('I`m functional flattner')
 }
+
+export default root

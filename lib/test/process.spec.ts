d2158bbc5bcc339b008bde10862aa75c3c6ad142
Max Kim <hwaseen@gmail.com>
2020-12-06 00:29:34 +0900

    fix:remove casing() method in process() method test case to operate test independently

--- lib/test/process.spec.ts
+++ lib/test/process.spec.ts
@@ -12,17 +12,18 @@ interface Friend {
   favoriteAnimal: Animal
 }
 
+// Premise that target object already camel-cased
 const mockData = {
-  user_id: 12424,
-  user_name: 'max',
-  user_age: 25,
-  user_profile: {
-    user_profile_text: 'I Love Zebra',
-    user_favorite_animal: { id: 3, animal_name: 'vulture' },
-    user_friends: [
-      { id: 12324, name: 'julie', favorite_animal: { id: 0, animal_name: 'tiger' } },
-      { id: 11424, name: 'michael', favorite_animal: { id: 1, animal_name: 'lion' } },
-      { id: 18924, name: 'shawn', favorite_animal: { id: 2, animal_name: 'monkey' } },
+  userId: 12424,
+  userName: 'max',
+  userAge: 25,
+  userProfile: {
+    userProfileText: 'I Love Zebra',
+    userFavoriteAnimal: { id: 3, animalName: 'vulture' },
+    userFriends: [
+      { id: 12324, name: 'julie', favoriteAnimal: { id: 0, animalName: 'tiger' } },
+      { id: 11424, name: 'michael', favoriteAnimal: { id: 1, animalName: 'lion' } },
+      { id: 18924, name: 'shawn', favoriteAnimal: { id: 2, animalName: 'monkey' } },
     ],
   },
 }
@@ -40,7 +41,7 @@ const processPlan = (target:Target) => ({
 
 describe('FlattenTarget.process() method should', () => {
   it('return a modified object according to process plan parameter.', (done) => {
-    const result = flattener(mockData).casing().process(processPlan).returnResult()
+    const result = flattener(mockData).process(processPlan).returnResult()
     expect(result).toEqual({
       userId: 1,
       userName: 'max',
